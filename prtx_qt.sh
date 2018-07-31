f91b647db173257e6a7c0e4f60d7ad4b5f56e765
printech <41024456+printech@users.noreply.github.com>
2018-07-31 13:28:17 -0400

    Add files via upload

---
+++ prtx_qt.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get install -y libtool libssl-dev
+sudo apt-get install -y libboost-all-dev libzmq3-dev
+sudo apt-get install -y libevent-dev libqrencode3
+sudo apt-get install -y libminiupnpc-dev unzip
+sudo add-apt-repository -y ppa:bitcoin/bitcoin
+sudo apt-get update
+sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+
+wget https://github.com/Printex-official/printex-core/releases/download/v1.0.0.0/lin-qt.zip
+unzip -o lin-qt.zip
+rm -rf lin-qt.zip
+chmod +x printex-qt
+sudo mv printex-qt /usr/local/bin
+printex-qt &
+
